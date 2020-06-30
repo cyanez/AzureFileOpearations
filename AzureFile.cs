c1ea6d42d93941e48602e3ecacc58b7af157c0ba
cyanez <chris.yanez@gmail.com>
2020-06-30 05:42:06 -0500

    Initial Commit

---
+++ AzureFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.Azure;
+
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.File;
+
+namespace AzureInitial
+{
+  class AzureFile
+  {
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AzureInitial
+{
+  class Program
+  {
+    static void Main(string[] args) {
+
+      Console.WriteLine("Initial Command!!!");   
+      Console.ReadLine();
+
+    }
+  
+  }
+}
